27b5e547a42e2d5ba61824bd3fd87bff64078a52
lgmccurdy <lgm5223@psu.edu>
2024-02-01 12:52:55 -0500

    updating .py file

--- python-nlp/python-test.py
+++ python-nlp/python-test.py
@@ -1,16 +1,18 @@
-with open("lgm-chatgbt-results-2.txt", "r") as file:
-    # Read the contents of the file
-    file_contents = file.read()
+import spacy
 
-    print(file_contents)
+# nlp = spacy.cli.download("en_core_web_sm")
+nlp = spacy.load('en_core_web_sm')
 
-    import spacy
+chatgbt = open('lgm-chatgbt-results-2.txt', 'r', encoding='utf8')
+words = chatgbt.read()
+wordstrings = str(words)
+print(wordstrings)
 
-   # nlp = spacy.cli.download("en_core_web_sm")
-    nlp = spacy.load('en_core_web_sm')
 
-    text = "Amelia donned a leather aviator suit, her heart pounding with excitement and nervousness."
-    doc = nlp(text)
 
-    for token in doc:
-        print(token.text, token.pos_, token.dep_)
+
+text = "Amelia donned a leather aviator suit, her heart pounding with excitement and nervousness."
+doc = nlp(text)
+
+for token in doc:
+    print(token.text, token.pos_, token.dep_)
